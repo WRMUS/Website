751821bfce5240cee44034790335a1970d85f9bc
devadamf <49660485+devadamf@users.noreply.github.com>
2020-01-11 17:11:36 -0600

    Almost duplicated header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,35 @@
-import { Component, OnInit, ViewEncapsulation  } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Location } from '@angular/common';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',  
+  templateUrl: './app.component.html',
   styleUrls: [
-               './homepage.css',
-               './breadcrumb.css',
-               './navbar.css',
-               './navbar-media.css'
-             ],   
-  encapsulation: ViewEncapsulation.None 
-  
-/*
-  styleUrls: ['./app.component.css'],
-*/
+    './homepage-media.css',
+    './homepage.css',
+    './breadcrumb.css',
+    './navbar.css',
+    './navbar-media.css'
+  ],
+  encapsulation: ViewEncapsulation.None
+
+  /*
+    styleUrls: ['./app.component.css'],
+  */
 })
 
 export class AppComponent {
   title = 'app works!';
   router;
-  constructor(private _router: Router ) {
+  constructor(private _router: Router) {
     this.router = _router;
   }
-  
-public getActiveClass(activeValue:boolean) {
-    if(activeValue){
-        return 'active';
+
+  public getActiveClass(activeValue: boolean) {
+    if (activeValue) {
+      return 'active';
     }
   }
-    
+
 }
